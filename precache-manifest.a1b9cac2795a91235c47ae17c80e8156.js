94b8d126942033895e50be8c55c1bc3f7643a628
denil20272 <denil20272@users.noreply.github.com>
2024-03-05 10:00:00 +0000

    deploy: 9ee1a62fd621a4ce66e25955e7852e8a096dacbf

---
+++ precache-manifest.a1b9cac2795a91235c47ae17c80e8156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8829bacab3cdaa00c60e72a87b02223d",
+    "url": "https://denil20272.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "c40d82a21a4d2d3ef11c",
+    "url": "https://denil20272.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "6f1614e6b77c93cf7290",
+    "url": "https://denil20272.github.io/task-management-frontend/static/js/2.8ae1a336.chunk.js"
+  },
+  {
+    "revision": "c40d82a21a4d2d3ef11c",
+    "url": "https://denil20272.github.io/task-management-frontend/static/js/main.b4de7a84.chunk.js"
+  },
+  {
+    "revision": "9b9888670d3cdb1f83b8",
+    "url": "https://denil20272.github.io/task-management-frontend/static/js/runtime~main.8afa552b.js"
+  }
+]);
